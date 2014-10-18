e8cbdcc607fe9d2a2c08aff66e6209dddb159667
Mustapha <mustapha.hamoui@gmail.com>
2014-10-18 09:26:25 +0000

    building home page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,7 +2,16 @@
 
 @section('content')
 
-
-
-
+<div id="slideshow" class="section">
+	[slideshow]
+</div>
+<div class="section">
+	[Reasons to choose aris]
+</div>
+<div class="section">
+	[Get in Touch]
+</div>
+<div class="section">
+	[a word from the principal] [upcoming events]
+</div>
 @stop

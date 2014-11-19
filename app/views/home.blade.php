6b0f77dea73b4ebd1cf0afc5c52868093a6e2c76
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2014-11-19 16:13:37 +0000

    refining of responsive grid and header

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,39 +6,23 @@
 
 <div class="section">
 	<div class="inner">
-		<div class="half">
-			<h3 class="aris">Welcome from the Principal</h3>
-			<p>Welcome to the Al-Rayan International School website. Recognized as a young and forward thinking school ARIS is an outstanding place to work and go to school. Our students are from over twenty different countries, are hard working and high achieving, our parents are supportive, our faculty is inspirational and our Board of Directors is very dedicated and strategically focused.</p>
-			<div class="arisbackground">
-				<p>This is a bit of information that may be highlighted</p>
-			</div>
-		</div>		
-
-		<div class="quarter">
-			<img src="http://placehold.it/400x200" alt="">
-			<h4>Recent News Item</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In commodi nam atque doloremque cumque molestias. Laudantium quas, placeat ut quidem.</p>
-		</div>
-
-		<div class="quarter last">
-			<img src="http://placehold.it/400x200" alt="">
-			<h4>Recent Event</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In commodi nam atque doloremque cumque molestias. Laudantium quas, placeat ut quidem.</p>
-		</div>
-	
-	</div>
-</div>
-
-<div class="section dk ">
-	<div class="inner">
-		<div class="half"><h2>Reasons to choose aris</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In commodi nam atque doloremque cumque molestias. Laudantium quas, placeat ut quidem.</p>
-		</div>
-
-		<div class="half last"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur repellendus incidunt minima molestias, minus quaerat quas cupiditate modi corporis? Cupiditate.</p></div>
-		
-	</div>
-</div>
+		<div class="leftColumn">
+			<h2 class="slogan">The Gateway to Knowledge</h2>
+			<p>Recognized as a young and forward thinking school ARIS is an outstanding place to work and go to school. <a href="">Learn More&nbsp;&rarr;</a></p>
+		</div> <!-- leftColumn -->
+			
+		<div class="rightColumn">	
+			<h2 class="latestNews">
+				Latest News
+			</h2>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div> <!-- rightColumn -->
+	</div> <!-- inner -->
+</div> <!-- section -->
 
 
 @stop

c2f6bac18f594861fc94fa76f66133bc2a5667f9
Mustapha on Server <mustapha.hamoui@gmail.com>
2015-01-15 05:35:57 -0500

    server bootstrap details added

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('musiMac.local','vbox','musMBA.local', 'MUSTAPHA-PC'),
+	'staging'=>array('aris')
 
 ));
 

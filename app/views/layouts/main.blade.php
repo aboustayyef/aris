347632f635e789218aa1e952aaa3a9ba6add9219
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-02-12 20:49:51 +0000

    Added info and tweaked css

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -7,7 +7,7 @@ use Aris\Node;
 	<meta charset="UTF-8">
 	<title>{{$title}}</title>
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<link rel="stylesheet" href="{{asset('/css/aris_home.css?v=1.01')}}">
+	<link rel="stylesheet" href="{{asset('/css/aris_home.css?v=1.02')}}">
 	<link rel="stylesheet" href="{{asset('/css/font-awesome.min.css')}}">
 </head>
 <body>

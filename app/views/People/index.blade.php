238a54ec117c4dca6fb9ae223a3dc9b7cfbcc93f
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-02-23 20:12:20 +0000

    added info

--- app/views/People/index.blade.php
+++ app/views/People/index.blade.php
@@ -23,16 +23,38 @@
 	<div id="content-area">
 
 		@if(Auth::Check())
+			<br>
 			<div id="adminCreate">
 				<a href="{{route('people.create')}}/?from={{Request::path()}}">Add New Person</a>
 			</div>
 		@endif
 
-		<div class="breadcrumbs">You are here: <a href="/">Home</a> > People</div>
+		<div class="breadcrumbs">
+			You are here: <a href="/">Home</a> > 
+			@if ($type == 'all')
+				People 
+			@else
+				<a href="/people">People</a> >{{$type}}</a>
+			@endif
+
+		</div>
 		
 		@if ($type == 'all')
 			<h1>Aris People</h1>
-			<p>Create here a manual index for 3 types of people</p>
+			<a href="/people/admin">
+				<h2>Administration</h2>
+			</a>
+				<p>School leadership and Administration</p>
+			<a href="/people/faculty">
+				<h2>Faculty</h2>
+			</a>
+				<p>Teachers and teaching assistants</p>
+
+			<a href="/people/staff">
+				<h2>Staff</h2>
+			</a>
+				<p>Non-teaching school staff</p>
+
 		@else 
 			<h1>Aris {{$type}}</h1>
 			{{View::make('people.partials.peopleList')->with('people', $people)}}

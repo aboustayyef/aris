a28b93d55529193008dda476ec240e49673a898c
Mustapha Hamoui <stayyef@Mustaphas-MacBook-Air.local>
2015-02-28 13:32:35 +0000

    navigations will now be cached

--- app/aris/Navigation.php
+++ app/aris/Navigation.php
@@ -10,78 +10,78 @@ class Navigation
 		# code...
 	}
 	public function renderFullNav(){
+			$fullnav ="";
 			// Top Nav (About us - etc)
-			echo '<ul class="sections">';
+			$fullnav .= '<ul class="sections">';
 			$sections = (new Node)->topLevel();
 			foreach ($sections as $key => $section) {
 				if ($section->hasChildren()) {
-					echo '<li>' . $section->name  ;
-					echo '<i class="fa fa-caret-down"></i>';
-					echo '<div class="navigation_wrapper">';
-						echo '<div class="nav_title"><h2>'.$section->name.'</h2></div>';
+					$fullnav .= '<li>' . $section->name  ;
+					$fullnav .= '<i class="fa fa-caret-down"></i>';
+					$fullnav .= '<div class="navigation_wrapper">';
+						$fullnav .= '<div class="nav_title"><h2>'.$section->name.'</h2></div>';
 
 						// Navigation Image
-						echo '<div class="section_image">';
-							echo '<img src="/img/featured/sections/'.$section->getLink().'.jpg">';
-						echo '</div>';
+						$fullnav .= '<div class="section_image">';
+							$fullnav .= '<img src="/img/featured/sections/'.$section->getLink().'.jpg">';
+						$fullnav .= '</div>';
 
 						// Navigation Description
-						echo '<div class="section_description">';
-							echo '<div class="desc">'.$section->excerpt.'</div>';
-						echo '</div>';
+						$fullnav .= '<div class="section_description">';
+							$fullnav .= '<div class="desc">'.$section->excerpt.'</div>';
+						$fullnav .= '</div>';
 
 						// Navigation Left panel
-						echo '<div class="section_nav_list">';
-							echo '<ul>';
+						$fullnav .= '<div class="section_nav_list">';
+							$fullnav .= '<ul>';
 							foreach ($section->children() as $key => $subsection) {
 								if ($subsection->hasChildren()) {
 									// Navigation right panel
-									echo '<li class="haschildren">' . $subsection->name ;
-										echo '<div class="section_subnav_list">';
-											echo '<ul>';
+									$fullnav .= '<li class="haschildren">' . $subsection->name ;
+										$fullnav .= '<div class="section_subnav_list">';
+											$fullnav .= '<ul>';
 											foreach ($subsection->children() as $key => $page) {
-												echo '<li><a href="/' . $page->getLink() . '">' . $page->name . '</a></li>';
+												$fullnav .= '<li><a href="/' . $page->getLink() . '">' . $page->name . '</a></li>';
 											}
-											echo '</ul>';
-										echo '</div>';// section_subnav_list
-									echo '</li>'; // haschildren
+											$fullnav .= '</ul>';
+										$fullnav .= '</div>';// section_subnav_list
+									$fullnav .= '</li>'; // haschildren
 								} else {
 
-									echo '<li class="nochildren"><a href="/' . $subsection->getLink() . '">'.$subsection->name.'</a></li>';
+									$fullnav .= '<li class="nochildren"><a href="/' . $subsection->getLink() . '">'.$subsection->name.'</a></li>';
 								}
 							}
-							echo '</ul>';
-						echo '</div>'; // section_nav_list
+							$fullnav .= '</ul>';
+						$fullnav .= '</div>'; // section_nav_list
 
-					echo '</div>'; // navigation_wrapper
+					$fullnav .= '</div>'; // navigation_wrapper
 				} else {
-					echo '<li><a href="/' . $section->getLink() . '">' . $section->name . '</a>' ;
+					$fullnav .= '<li><a href="/' . $section->getLink() . '">' . $section->name . '</a>' ;
 				}
-				echo '</li>';
+				$fullnav .= '</li>';
 			}
-			echo '</ul>'; //sections
+			$fullnav .= '</ul>'; //sections
+			return $fullnav;
 		}
 
 	public function renderMobileNav(){
-		echo '<div id="mobileMenu"><h3><i class="fa fa-bars"></i> Menu</h3>';
-		echo '<ul class="sectionsMobile">';
+		$mobilenav = "";
 		$sections = (new Node)->topLevel();
 		foreach ($sections as $key => $section) {
-			echo '<li><h4>' . $section->name . '</h4>';
+			$mobilenav .= '<li><h4>' . $section->name . '</h4>';
 
-			echo '<ul class="subsections">';
+			$mobilenav .= '<ul class="subsections">';
 
 			$subsections = $section->children();
 			foreach ($subsections as $key => $subsection) {
-				echo '<li>';
-				echo '<a href="/'.$subsection->getLink().'">' . $subsection->name . '</a>';
-				echo '</li>';
+				$mobilenav .= '<li>';
+				$mobilenav .= '<a href="/'.$subsection->getLink().'">' . $subsection->name . '</a>';
+				$mobilenav .= '</li>';
 			}
-			echo '</ul>';
-			echo '</li>';
+			$mobilenav .= '</ul>';
+			$mobilenav .= '</li>';
 		}
-		echo '<ul>';
-		echo '</div>';
+		return $mobilenav;
 	}
 
 
--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -1,10 +1,35 @@
 
 <?php 
 	$navigation = new Aris\Navigation;
-	echo '<nav>';
-		echo '<div class="inner">';
-			$navigation->renderFullNav();
-		echo '</div>';
-	echo '</nav>';
-		$navigation->renderMobileNav();
 ?>
+	<nav>
+		<div class="inner">
+
+			<?php 
+			if ( ! Cache::has('fullnav')) {
+				Cache::forever('fullnav', $navigation->renderFullNav());
+			}
+
+			echo Cache::get('fullnav');
+
+			?>
+			
+		</div>
+	</nav>
+
+	<!-- Mobile Navigation -->
+	
+	<div id="mobileMenu">
+		<h3><i class="fa fa-bars"></i> Menu</h3>
+		<ul class="sectionsMobile">
+			<?php 
+			if ( ! Cache::has('mobilenav')) {
+				Cache::forever('mobilenav', $navigation->renderMobileNav());
+			}
+
+			echo Cache::get('mobilenav');
+
+			?>
+			{{$navigation->renderMobileNav()}}
+		</ul>
+	</div>

0f03684640f67a08b9fdbf273aba4010dd19bf22
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-03-28 00:38:40 +0000

    implemented tabbed sectioning of pages

--- public/js/aris.js
+++ public/js/aris.js
@@ -1,4 +1,6 @@
 // @codekit-append "aris/_navigation.js"
 // @codekit-append "aris/_navigation_mobile.js"
+// @codekit-append "aris/_tabs.js"
+
 // find a better place for this later 
 $('.flexslider').flexslider();

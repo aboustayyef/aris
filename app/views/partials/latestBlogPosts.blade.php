b6cb581b8dea0b03c083a9a133c5cad51d19ca54
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-03-15 21:04:39 +0000

    added another school blog

--- app/views/partials/latestBlogPosts.blade.php
+++ app/views/partials/latestBlogPosts.blade.php
@@ -3,11 +3,11 @@
 // Prepare SimplePie
 
 $sp = new SimplePie();
-$sp->set_feed_url('https://arisprimaryict.wordpress.com/feed');
+$sp->set_feed_url(['https://arisprimaryict.wordpress.com/feed', 'https://nikasemo.wordpress.com/feed']);
 $sp->set_cache_duration(1800);
 $sp->set_cache_location(storage_path().'/spcache');
 $sp->init();
-$latestPosts = $sp->get_items(0,3);
+$latestPosts = $sp->get_items(0,4);
 ?>
 
 <h2 class="latestNews">
@@ -17,7 +17,11 @@ $latestPosts = $sp->get_items(0,3);
 @foreach ($latestPosts as $post)
 <div class="news_item_wrapper">
 <div class="news_item_photo">
-	<img src="https://arisprimaryict.files.wordpress.com/2016/01/arisictlogo3.png" width="60" height="auto" alt="">
+	@if( strpos($post->get_link(), 'arisprimaryict.wordpress.com'))
+		<img src="https://arisprimaryict.files.wordpress.com/2016/01/arisictlogo3.png" width="60" height="auto" alt="">
+	@else
+		<img src="/img/wordpress-logo.png" width="60" height="auto" alt="wordpress logo">
+	@endif
 </div>
 	<a href="{{$post->get_link()}}">
 		<h3>{{$post->get_title()}}</h3>

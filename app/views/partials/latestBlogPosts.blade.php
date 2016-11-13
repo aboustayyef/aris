430d1714b84bd57d363ff7385f78dc01660ebb65
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-11-13 08:34:50 +0000

    improved the latest blog posts section

--- app/views/partials/latestBlogPosts.blade.php
+++ app/views/partials/latestBlogPosts.blade.php
@@ -3,11 +3,38 @@
 // Prepare SimplePie
 
 $sp = new SimplePie();
-$sp->set_feed_url(['https://arisprimaryict.wordpress.com/feed', 'https://nikasemo.wordpress.com/feed']);
+$sp->set_feed_url(
+	[
+		'https://arisprimaryict.wordpress.com/feed',
+		'https://nikasemo.wordpress.com/feed',
+		'https://parentsblog.aris.edu.gh/feed'
+	]
+);
 $sp->set_cache_duration(1800);
 $sp->set_cache_location(storage_path().'/spcache');
 $sp->init();
 $latestPosts = $sp->get_items(0,4);
+
+$blog_definitions = 
+[
+	[
+		'unique_identifier'		=>	'arisprimaryict.wordpress.com',
+		'thumb_location'		=>	'https://arisprimaryict.files.wordpress.com/2016/01/arisictlogo3.png',
+		'blog_title'			=>	'The Online Journal of ICT at ARIS Primary'
+	],
+	[
+		'unique_identifier'		=>	'parentsblog.aris.edu.gh',
+		'thumb_location'		=>	'/img/aris-parents-logo.png',
+		'blog_title'			=>	'ARIS Parents Blog'
+	],
+	[
+		'unique_identifier'		=>	'nikasemo.wordpress.com',
+		'thumb_location'		=>	'/img/nikasemo.png',
+		'blog_title'			=>	'Our Learning Experiences (Nikasemo)'
+	],
+
+]
+
 ?>
 
 <h2 class="latestNews">
@@ -16,18 +43,18 @@ $latestPosts = $sp->get_items(0,4);
 
 @foreach ($latestPosts as $post)
 <div class="news_item_wrapper">
-<div class="news_item_photo">
-	@if( strpos($post->get_link(), 'arisprimaryict.wordpress.com'))
-		<img src="https://arisprimaryict.files.wordpress.com/2016/01/arisictlogo3.png" width="45" height="auto" alt="">
-	@else
-		<img src="/img/wordpress-logo.png" width="45" height="auto" alt="wordpress logo">
-	@endif
-</div>
-	<a href="{{$post->get_link()}}">
-		<h3>{{$post->get_title()}}</h3>
-	</a>
-	<div class="news_excerpt">
-		<p>{{$sp->get_title()}}</p>
-	</div>
+	@foreach($blog_definitions as $definition)
+		@if( strpos($post->get_link(), $definition['unique_identifier']))
+			<div class="news_item_photo">
+						<img src="{{$definition['thumb_location']}}" width="45" height="auto" alt="">
+			</div>
+			<a href="{{$post->get_link()}}">
+				<h3>{{$post->get_title()}}</h3>
+			</a>
+			<div class="news_excerpt">
+				<p>{{$definition['blog_title']}}</p>
+			</div>
+		@endif
+	@endforeach
 </div>
 @endforeach

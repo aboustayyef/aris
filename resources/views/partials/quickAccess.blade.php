63e2b4582969cd6f0f07fe49b7a9c8ecf2b3a8ba
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-13 17:03:23 +0000

    replaced apply with Enroll Now and made button pop out more

--- resources/views/partials/quickAccess.blade.php
+++ resources/views/partials/quickAccess.blade.php
@@ -1,4 +1,4 @@
 <div class="quickAccess">
 	<h2>Quick Access Links</h2>
 	<ul>
-		<a href="{{Url('admissions/admissions-procedure')}}"><li> <i class="fa fa-file-o"></i>Apply</li></a><a href="{{Url('contact-us')}}"><li> <i class="fa fa-phone"></i>Contact</li></a><a href="{{Url('contact-us')}}"><li><i class="fa fa-map-marker"></i>Map</li></a><a href="{{Url('calendar')}}"><li><i class="fa fa-calendar"></i>Academic Calendar</li></a><a href="{{Url('news')}}"><li><i class="fa fa-list-ul"></i>News</li></a></ul></div>
+		<a href="http://apply.aris.edu.gh"><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li></a><a href="{{Url('contact-us')}}"><li> <i class="fa fa-phone"></i>Contact</li></a><a href="{{Url('contact-us')}}"><li><i class="fa fa-map-marker"></i>Map</li></a><a href="{{Url('calendar')}}"><li><i class="fa fa-calendar"></i>Academic Calendar</li></a><a href="{{Url('news')}}"><li><i class="fa fa-list-ul"></i>News</li></a></ul></div>

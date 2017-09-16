104f6a36fd02287650a62d4ba52c0aecdd34757b
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-16 15:21:47 +0000

    adjust slide selection algorithm to be able to skip slides

--- resources/views/partials/slideshow.blade.php
+++ resources/views/partials/slideshow.blade.php
@@ -4,15 +4,16 @@
 	
 	//settings
 	
-	$slidesAvailable = 13;
-	$slidesToShow = 5;
+	$slidesAvailable = 16;
+	$slidesToSkip = [1,3,9,12];
+	$slidesToShow = 6;
 	$slideSet = [];
 
 	// Create random set;
 
 	while (count($slideSet) < $slidesToShow) {
 		$candidate = mt_rand(1, $slidesAvailable);
-		if (!in_array($candidate, $slideSet)) {
+		if (!in_array($candidate, $slideSet) && !in_array($candidate, $slidesToSkip)) {
 			$slideSet[] = $candidate;
 		}
 	}
@@ -30,7 +31,7 @@
 	   	@foreach ($slideSet as $key => $slideNumber)
 			
 			<li>
-				<img src="{{asset('img/slides/slide' . $slideNumber . '.jpg')}}" alt="">
+				<img src="{{asset('img/slides/slide' . $slideNumber . '.jpg?v=2')}}" alt="">
 			</li>
 
 	   	@endforeach

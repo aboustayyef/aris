7416c05e5c4d60b356f05e3826291b5c6d6fc8b2
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-17 12:48:57 +0000

    added book a tour button

--- resources/views/partials/quickAccess.blade.php
+++ resources/views/partials/quickAccess.blade.php
@@ -1,4 +1,23 @@
 <div class="quickAccess">
-	<h2>Quick Access Links</h2>
-	<ul>
-		<a href="http://apply.aris.edu.gh"><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li></a><a href="{{Url('contact-us')}}"><li> <i class="fa fa-phone"></i>Contact</li></a><a href="{{Url('contact-us')}}"><li><i class="fa fa-map-marker"></i>Map</li></a><a href="{{Url('calendar')}}"><li><i class="fa fa-calendar"></i>Academic Calendar</li></a><a href="{{Url('news')}}"><li><i class="fa fa-list-ul"></i>News</li></a></ul></div>
+   <h2>Quick Access Links</h2>
+   <ul>
+      <a href="http://apply.aris.edu.gh">
+         <li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li>
+      </a>
+      <a href="{{Url('contact-us')}}">
+         <li> <i class="fa fa-phone"></i>Contact</li>
+      </a>
+      <a href="{{Url('contact-us')}}">
+         <li><i class="fa fa-map-marker"></i>Map</li>
+      </a>
+      <a href="{{Url('calendar')}}">
+         <li><i class="fa fa-calendar"></i>Academic Calendar</li>
+      </a>
+      <a href="{{Url('news')}}">
+         <li><i class="fa fa-list-ul"></i>News</li>
+      </a>
+       <a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour">
+         <li><i class="fa fa-list-ul"></i>Book A Tour</li>
+      </a>
+   </ul>
+</div>

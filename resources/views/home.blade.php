6d0149d4b9c79e92ae36c511ab7d11b5944e9632
Mustapha on Server <mustapha.hamoui@gmail.com>
2017-09-19 09:46:29 +0000

    changed video on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,8 +29,7 @@ use Aris\News;
 			</div>
 			<div>
 				<div class="videoWrapper">
-					<iframe src="https://player.vimeo.com/video/222509969?byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<p><a href="https://vimeo.com/222509969">IB Global Conference, The Hague 2017</a> from <a href="https://vimeo.com/iboorg">International Baccalaureate</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+                    <iframe src="https://player.vimeo.com/video/221415953" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 				</div>		
 				{{-- @include('partials.homePagePhotoRotate') --}}
 			</div>

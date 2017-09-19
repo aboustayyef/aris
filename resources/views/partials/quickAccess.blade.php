5bed7fb5624fb51b59a62467caa130d33cca2e7c
Mustapha on Server <mustapha.hamoui@gmail.com>
2017-09-19 09:40:53 +0000

    made book a tour button teal

--- resources/views/partials/quickAccess.blade.php
+++ resources/views/partials/quickAccess.blade.php
@@ -4,6 +4,9 @@
       <a href="http://apply.aris.edu.gh">
          <li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li>
       </a>
+       <a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour">
+         <li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"><i class="fa fa-list-ul"></i>Book A Tour</li>
+      </a>
       <a href="{{Url('contact-us')}}">
          <li> <i class="fa fa-phone"></i>Contact</li>
       </a>
@@ -15,9 +18,6 @@
       </a>
       <a href="{{Url('news')}}">
          <li><i class="fa fa-list-ul"></i>News</li>
-      </a>
-       <a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour">
-         <li><i class="fa fa-list-ul"></i>Book A Tour</li>
       </a>
    </ul>
 </div>

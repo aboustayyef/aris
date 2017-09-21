ea9715c33f8546de3e119614bdcdb5f205c32227
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-21 20:48:21 +0000

    cleaned up quick access buttons

--- resources/views/partials/quickAccess.blade.php
+++ resources/views/partials/quickAccess.blade.php
@@ -1,25 +1,5 @@
 <div class="quickAccess">
    <h2>Quick Access Links</h2>
-   <ul>
-      <a href="http://apply.aris.edu.gh">
-         <li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li>
-      </a>
-       <a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour"
-         onclick="ga('send', 'event', 'Outgoing Link', 'Book A Tour')">
-         
-         <li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"><i class="fa fa-list-ul"></i>Book A Tour</li>
-      </a>
-      <a href="{{Url('contact-us')}}">
-         <li> <i class="fa fa-phone"></i>Contact</li>
-      </a>
-      <a href="{{Url('contact-us')}}">
-         <li><i class="fa fa-map-marker"></i>Map</li>
-      </a>
-      <a href="{{Url('calendar')}}">
-         <li><i class="fa fa-calendar"></i>Academic Calendar</li>
-      </a>
-      <a href="{{Url('news')}}">
-         <li><i class="fa fa-list-ul"></i>News</li>
-      </a>
+   <ul><a href="http://apply.aris.edu.gh"><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li></a><a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour" onclick="ga('send', 'event', 'Outgoing Link', 'Book A Tour')"</a><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"><i class="fa fa-list-ul"></i>Book A Tour</li></a><a href="{{Url('contact-us')}}"><li> <i class="fa fa-phone"></i>Contact</li></a><a href="{{Url('contact-us')}}"><li><i class="fa fa-map-marker"></i>Map</li></a><a href="{{Url('calendar')}}"><li><i class="fa fa-calendar"></i>Academic Calendar</li></a><a href="{{Url('news')}}"><li><i class="fa fa-list-ul"></i>News</li>
    </ul>
 </div>

62ee52d46a369a074e5417c8f59b9431d3a1c216
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-23 07:51:37 +0000

    Added analytics to admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,6 +36,21 @@
         </div>
     </div>
 
+    {{-- Analytics --}}
+    <script src="/js/aris_main.95382d7ec283220d3674.js"></script>
+    <!-- Analytics -->
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-61584501-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     <!-- Scripts -->
     <script src="{{ mix('js/aris_admin.js') }}"></script>
 </body>

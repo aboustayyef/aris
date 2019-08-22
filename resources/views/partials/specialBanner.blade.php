d55d04658bcac56d894f1e9d8d8e845fb1118dc6
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-08-22 18:38:12 +0000

    fixed join the team url bug

--- resources/views/partials/specialBanner.blade.php
+++ resources/views/partials/specialBanner.blade.php
@@ -1,3 +1,3 @@
 <div class="special_banner">
-	<strong>Join the team:</strong> <a href="/about-us/join">ARIS is recruiting</a> 
+	<a href="/about-us/join"><strong>Join the team:</strong> ARIS is recruiting</a> 
 </div>

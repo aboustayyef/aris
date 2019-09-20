f00a5d365c3fc2d6a1bff9bbf4004a0310de9914
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-09-20 17:34:57 +0000

    replaced graduating class in slide with 2019 class

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
 	</div>
    <ul class="slides">
    		{{-- special pinned slide --}}
-   		<li><img src="{{asset('img/slides/aris_graduating_class_2018.jpg?v=2')}}" alt=""></li>
+   		<li><img src="{{asset('img/slides/aris_graduating_class_2019.jpg')}}" alt=""></li>
 	   	@foreach ($slides as $key => $slide)
 			<li><img src="{{asset('img/slides/' . $slide . '?v=2')}}" alt=""></li>
 	   	@endforeach

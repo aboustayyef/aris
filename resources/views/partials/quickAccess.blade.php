a049ce0e42ef050e739625c8a0bf74ade110da73
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-09-29 13:15:07 +0000

    Work in progress

--- resources/views/partials/quickAccess.blade.php
+++ resources/views/partials/quickAccess.blade.php
@@ -1,5 +1,31 @@
 <div class="quickAccess">
    <h2>Quick Access Links</h2>
-   <ul><a href="http://apply.aris.edu.gh"><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"> <i class="fa fa-file-o"></i>Enroll Now</li></a><a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour" onclick="ga('send', 'event', 'Outgoing Link', 'Book A Tour')"</a><li style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"><i class="fa fa-list-ul"></i>Book A Tour</li></a><a href="{{Url('contact-us')}}"><li> <i class="fa fa-phone"></i>Contact</li></a><a href="{{Url('contact-us')}}"><li><i class="fa fa-map-marker"></i>Map</li></a><a href="{{Url('calendar')}}"><li><i class="fa fa-calendar"></i>Academic Calendar</li></a><a href="{{Url('news')}}"><li><i class="fa fa-list-ul"></i>News</li>
+   <ul>
+   <li><a href=""><i
+               class="fa fa-file-o"></i> info </a> </li>
+   <li class="highlighted" ><a href=""> info </a> </li>
+   <li><a href=""><i
+               class="fa fa-file-o"></i> info </a> </li>
+   <li><a href=""><i
+               class="fa fa-file-o"></i> info </a> </li>
+   <li><a href=""><i
+               class="fa fa-file-o"></i> info </a> </li>
+   <li><a href=""><i
+               class="fa fa-file-o"></i>info </a> </li>
+
+      <a href="http://apply.aris.edu.gh">
+         <li class="highlighted">&nbsp;<i
+               class="fa fa-file-o"></i>Enroll Now</li>
+      </a><a href="mailto:communication@aris.edu.gh?subject=Request for ARIS Tour"
+         onclick="ga('send', 'event', 'Outgoing Link', 'Book A Tour')" </a> <li
+         style="background-color: #0097a1;color: white;padding: 4px 16px; border: 1px white solid;"><i
+            class="fa fa-list-ul"></i>Book A Tour</li></a><a href="{{Url('contact-us')}}">
+         <li> <i class="fa fa-phone"></i>Contact</li>
+      </a><a href="{{Url('contact-us')}}">
+         <li><i class="fa fa-map-marker"></i>Map</li>
+      </a><a href="{{Url('calendar')}}">
+         <li><i class="fa fa-calendar"></i>Academic Calendar</li>
+      </a><a href="{{Url('news')}}">
+         <li><i class="fa fa-list-ul"></i>News</li>
    </ul>
 </div>

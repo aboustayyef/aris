6a3abb0d7cccdcbe74eea927f45a51fd71b80184
Mustapha on Server <mustapha.hamoui@gmail.com>
2020-08-12 10:22:06 +0000

    updated covid-19 banner message

--- resources/views/partials/virtual_learning_banner.blade.php
+++ resources/views/partials/virtual_learning_banner.blade.php
@@ -50,7 +50,7 @@
         <div>
             <div class="grid1-2">
                 <div class="notice">Notice:</div>
-                <h2>Our Campus is Closed. <br>Our Learning Continues.</h2>
+                <h2>Welcome Back!<br>Our Virtual Learning Continues</h2>
                 {{-- <p>We are virtually learning at this time while our campus is under a government closure decree. Our learning continues, check the ARIS Virtual Learning Programme page (under construction) to know more about how students will remotely continue to study during this time.</p> --}}
                 <p style="font-size:21px">Visit the <a href="/news/2020-march-aris-moves-to-virtual-learning-due-to-the-covid-19-arrival-in-ghana">ARIS Virtual Learning Information </a>page to get updates on Covid-19, read stories, show appreciation, improve your wellbeing, and more!</p>
             </div>

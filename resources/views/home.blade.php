240f5fdd3bc0ff3700ae3c44079568957081301c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2020-10-14 09:22:07 +0000

    added Japan Motors Logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.main')
 
 @section('title')
-	<meta name="Description" content="As a forward thinking school, our mission focuses on the provision of the highest level of International standards in education adopting the Cambridge International and British National Curricula.">
-	<title>Al-Rayan International School - Ghana</title>
+<meta name="Description"
+	content="As a forward thinking school, our mission focuses on the provision of the highest level of International standards in education adopting the Cambridge International and British National Curricula.">
+<title>Al-Rayan International School - Ghana</title>
 @stop
 
 @section('facebookMeta')
-	<!-- Facebook Open Graph Data -->
-    <meta property="og:title" content="Al-Rayan International School, Ghana">
-    <meta property="og:description" content="As a forward thinking school, our mission focuses on the provision of the highest level of International standards in education adopting the Cambridge International and British National Curricula.">
-    <meta property="og:image" content="https://aris.edu.gh/img/slides/slide1.jpg">
+<!-- Facebook Open Graph Data -->
+<meta property="og:title" content="Al-Rayan International School, Ghana">
+<meta property="og:description"
+	content="As a forward thinking school, our mission focuses on the provision of the highest level of International standards in education adopting the Cambridge International and British National Curricula.">
+<meta property="og:image" content="https://aris.edu.gh/img/slides/slide1.jpg">
 @stop
 
 @section('content')
 
 <!-- Slide Show -->
 <div class="flexslider">
-   <ul class="slides">
-   		{{-- special pinned slide --}}
-   		<li><img src="{{asset('img/slides/aris_graduating_class_2019.jpg')}}" alt=""></li>
-   		<li><img src="{{asset('img/slides/ibcp_all_four_programs.png')}}" alt=""></li>
-	   	@foreach ($slides as $key => $slide)
-			<li><img src="{{asset('img/slides/' . $slide . '?v=2')}}" alt=""></li>
-	   	@endforeach
-  </ul>
+	<ul class="slides">
+		{{-- special pinned slide --}}
+		<li><img src="{{asset('img/slides/aris_graduating_class_2019.jpg')}}" alt=""></li>
+		<li><img src="{{asset('img/slides/ibcp_all_four_programs.png')}}" alt=""></li>
+		@foreach ($slides as $key => $slide)
+		<li><img src="{{asset('img/slides/' . $slide . '?v=2')}}" alt=""></li>
+		@endforeach
+		  
+	</ul>
 	<div class="inspirewrap">
 		<div class="inspireempowertransform">Inspire, Empower,Transform</div>
 	</div>
@@ -35,17 +38,22 @@
 			<div class="grid1-2">
 				<div class="intro">
 					<h2 class="slogan">Welcome to ARIS</h2>
-					<p>Recognized as a young and forward-thinking school, ARIS is an outstanding place for students and teachers alike. <a href="{{url('/about-us/why-aris')}}" class="learnmore">Learn More&nbsp;&rarr;</a></p>
+					<p>Recognized as a young and forward-thinking school, ARIS is an outstanding place for students and
+						teachers alike. <a href="{{url('/about-us/why-aris')}}" class="learnmore">Learn
+							More&nbsp;&rarr;</a></p>
 				</div>
 
 				@include('partials.latestNews')
 				@include('partials.latestVideos')
-
+				<h2 class="latestNews">Thank You</h2>
+				<a href="https://www.japanmotors.com/">
+					<img src="/img/japan_motors_logo_820x206.png" width="410px" alt="Japan Motors Logo">
+				</a>
 				
 			</div> <!-- grid1-2 -->
-			
+
 			<div class="grid2-2">
-				
+
 				@include('partials.quickAccess')
 				@include('partials.arisCredentials')
 				{{-- @include('partials.arisRecruiting') --}}
@@ -57,22 +65,23 @@
 		</div>
 		<div>
 			<div class="grid1-2">
-			</div>	
+			</div>
 			<div class="grid2-2">
 				{{-- Empty --}}
 			</div>
-			</div>
 		</div>
-	</div> <!-- inner -->
+	</div>
+</div> <!-- inner -->
 </div> <!-- section -->
 <div class="section">
 	<div class="inner" style="margin-bottom:3.5em">
 		<hr style="margin-bottom:1.5em;border:1px solid #ededed">
 		<div class="grid2-2">
-			<a href="http://www.cambridgeinternational.org/"><img src= {{asset('/img/cambridge.png')}} width="50%" height="50%"></a>
+			<a href="http://www.cambridgeinternational.org/"><img src={{asset('/img/cambridge.png')}} width="50%"
+					height="50%"></a>
 		</div>
 		<div class="grid1-2">
-				<p>ARIS is a registered Associate of Cambridge Assessment International Education</p>
+			<p>ARIS is a registered Associate of Cambridge Assessment International Education</p>
 		</div>
 	</div>
 </div>
